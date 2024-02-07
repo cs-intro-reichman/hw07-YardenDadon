bdb72b7b350586997a6c6ea79f9269bfe9bbbce9
Yarden Dadon <yarden.dadon@post.runi.ac.il>
2024-02-08 00:55:01 +0200

    iloverecursions

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -4,11 +4,11 @@ public class HashTagTokenizer {
 
 	public static void main(String[] args) {
 
-		//String hashTag = args[0];
+		String hashTag = args[0];
 		String []dictionary = readDictionary("dictionary.txt");
-		System.out.println(dictionary[2967]); 
-		System.out.print(existInDictionary("word", dictionary));
-		//breakHashTag(hashTag, dictionary);
+	//	System.out.println(dictionary[2967]); 
+	//	System.out.print(existInDictionary("word", dictionary));
+		breakHashTag(hashTag, dictionary);
 	}
 
 	public static String[] readDictionary(String fileName) {
@@ -29,17 +29,20 @@ public class HashTagTokenizer {
 	}
 	
 	public static void breakHashTag(String hashtag, String[] dictionary) {
-
-		// Base case: do nothing (return) if hashtag is an empty string.
-        if (hashtag.isEmpty()) {
+		String lowercase = hashtag.toLowerCase();
+        if (hashtag.isEmpty()) {// Base case: do nothing (return) if hashtag is an empty string.
             return;
         }
- 
         int N = hashtag.length();
-
         for (int i = 1; i <= N; i++) {
-		
+			if (existInDictionary(lowercase.substring(0, i), dictionary) == true){
+				System.out.println(lowercase.substring(0, i));
+				breakHashTag(lowercase.substring(i), dictionary);
+				return;
+			}
+
+		}
         }
     }
 
-}
+
