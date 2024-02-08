e0b458511ce81bd3b73748afcdce45cbb5b68f0b
Yarden Dadon <yarden.dadon@post.runi.ac.il>
2024-02-08 02:43:54 +0200

    Done>

--- SpellChecker.java
+++ SpellChecker.java
@@ -21,14 +21,15 @@ public class SpellChecker {
 		int word1Length = word1.length();
 		int word2Length = word2.length();
 		if (word1Length == 0){return word2Length;}
-		if (word2Length == 0){return word1Length;}
-		if (lowercase1.charAt(0) == lowercase2.charAt(0)) {
+		else if (word2Length == 0){return word1Length;}
+		else if (lowercase1.charAt(0) == lowercase2.charAt(0)) {
 			return levenshtein(tail(lowercase1), tail(lowercase2));
-		} 
+		} else {
 		int first = levenshtein(tail(lowercase1), tail(lowercase2));
 		int second = levenshtein(lowercase1, tail(lowercase2));
 		int third = levenshtein(tail(lowercase1), lowercase2);
 		return 1 + Math.min(Math.min(first, second), third);
+		}
 	}
 	/////////////////////////////////////////////////////////////
 	public static String[] readDictionary(String fileName) {
@@ -42,7 +43,25 @@ public class SpellChecker {
 	}
 	/////////////////////////////////////////////////////////////
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
+		String minWord = "";
+		for (var i = 0; i < dictionary.length; i++) {
+			if (levenshtein(word, dictionary[i]) <= levenshtein(word, minWord)) {
+				if (levenshtein(word, minWord) > threshold) {
+					minWord = dictionary[i];
+				}
+			}
+
+			// Check the case where if the input word is a word in the dictionary, then it is correct and we save minWord to that word.
+			if (word.equals(dictionary[i])) {
+				minWord = dictionary[i];
+			}
+			//System.out.println(dictionary[i] + " " + minWord + " " + levenshtein(word, minWord));
+		}
+
+		if (levenshtein(word, minWord) > threshold) {
+			return word;
+		}
+		return minWord;
 	}}
 
 
