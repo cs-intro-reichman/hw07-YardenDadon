c10b80ac92653121dfe1e6e57e6771b686cf4cf6
Yarden Dadon <yarden.dadon@post.runi.ac.il>
2024-02-08 02:26:17 +0200

    SHe Livin on my Shtein

--- SpellChecker.java
+++ SpellChecker.java
@@ -19,7 +19,6 @@ public class SpellChecker {
 	public static int levenshtein(String word1, String word2) {
 		String lowercase1 = word1.toLowerCase();
 		String lowercase2 = word2.toLowerCase();
-		int count = 0;
 		int word1Length = word1.length();
 		int word2Length = word2.length();
 		if (word1Length == 0){return word2Length;}
@@ -27,18 +26,20 @@ public class SpellChecker {
 
 
 		
-			if (lowercase1.substring(0, 1).equals(lowercase2.substring(0, 1))){
-				levenshtein(tail(lowercase1), tail(lowercase2));
-			}else {return 1 + Math.min(levenshtein(tail(lowercase1), lowercase2), 
+		if (lowercase1.charAt(0) == lowercase2.charAt(0)) {
+			return levenshtein(tail(lowercase1), tail(lowercase2));
+			
+		} else {return 1 + Math.min(levenshtein(tail(lowercase1), lowercase2), 
 				Math.min(levenshtein(lowercase1, tail(lowercase2)), 
 				levenshtein(tail(lowercase1), tail(lowercase2))));}
 
 
 		}
-
-
-
 	}
+
+
+
+	
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
